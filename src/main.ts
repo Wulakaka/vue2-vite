968fe51b1e962ef3b0e8f07ce1da779c905768c5
张伟峰 <zhang.weifeng@tslsmart.com>
2022-10-25 18:29:42 +0800

    feat: 增加子应用接入

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,64 @@ import Vue from 'vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+import routes from './router/routes'
+import VueRouter from 'vue-router'
 
 import './assets/main.css'
-
+import {
+  renderWithQiankun,
+  qiankunWindow,
+} from 'vite-plugin-qiankun/dist/helper'
 Vue.use(PiniaVuePlugin)
+Vue.use(VueRouter)
 
-new Vue({
-  router,
-  pinia: createPinia(),
-  render: (h) => h(App)
-}).$mount('#app')
+let router = null
+let instance: null | Vue = null
+
+type Props = object & { container?: HTMLElement }
+
+function render(props: Props = {}) {
+  const { container } = props
+  router = new VueRouter({
+    base: qiankunWindow.__POWERED_BY_QIANKUN__ ? '/micro/vue2-vite/' : '/',
+    mode: 'history',
+    routes,
+  })
+
+  instance = new Vue({
+    router,
+    pinia: createPinia(),
+    render: (h) => h(App),
+  }).$mount(container ? container.querySelector('#app') || '#app' : '#app')
+}
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped')
+}
+export async function mount(props: Props) {
+  console.log('[vue] props from main framework', props)
+  render(props)
+}
+export async function unmount() {
+  if (instance) {
+    instance.$destroy()
+    instance.$el.innerHTML = ''
+    instance = null
+    router = null
+  }
+}
+
+// some code
+renderWithQiankun({
+  mount,
+  bootstrap,
+  unmount,
+  update() {
+    console.log('update')
+  },
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render()
+}
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 import vue2Jsx from '@vitejs/plugin-vue2-jsx'
 
+import qiankun from 'vite-plugin-qiankun'
+
+const useDevMode = true
+import { name } from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,7 +18,12 @@ export default defineConfig({
       targets: ['ie >= 11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
     }),
+    // 这里的 'vue2-vite' 是子应用名，主应用注册时AppName需保持一致
+    qiankun(name, {
+      useDevMode,
+    }),
   ],
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
